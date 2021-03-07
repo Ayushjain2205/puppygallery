2162576a7e09d850a54143a7261d65fa60695570
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-03-07 19:25:04 +0530

    final changes

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Title = () => {
   return (
-    <div className="title">
+    <div className='title'>
+      <img className='logo_image' src={require("../doggo.png")} />
       <h1>PuppyGallery</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Your Cute Corgi Pictures</h2>
+      <p>Upload all your cute puppy pictures and build your collection!</p>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
